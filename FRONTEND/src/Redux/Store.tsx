e741ae06b6a49e48413e4728ee1e569a5a13f2b6
AAYUS KARKI <alexkarki2060@gmail.com>
2024-05-10 18:30:02 +0545

    Store persisted!

--- FRONTEND/src/Redux/Store.tsx
+++ FRONTEND/src/Redux/Store.tsx
@@ -1,15 +1,38 @@
-import { configureStore as ConfigureStore } from "@reduxjs/toolkit"
+import { configureStore as ConfigureStore, combineReducers } from "@reduxjs/toolkit"
 import UserReducer from './Userslice'
 import Otheruser from './Otheruser'
 import MessageReducer from './Message'
 import selectedUser from './Selecteduser'
-const store = ConfigureStore({
-    reducer: {
-        user: UserReducer,
-        otheruser: Otheruser,
-        message: MessageReducer,
-        selecteduser: selectedUser
-    }
-})
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+
+const rootreducer = combineReducers({
+    user:persistReducer(persistConfig, UserReducer),
+    otheruser:persistReducer(persistConfig, Otheruser),
+    message:persistReducer(persistConfig, MessageReducer),
+    selecteduser:persistReducer(persistConfig, selectedUser)
+})
+const store = ConfigureStore({
+    reducer:rootreducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 export default store
--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -4,13 +4,17 @@ import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
 import store from './Redux/Store.tsx'
 import App from './App.tsx'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 <Provider store={store}>
+<PersistGate loading={null} persistor={persistStore(store)}>
     <Toaster />
     <App />
+    </PersistGate>
 </Provider>
   </React.StrictMode>,
 )
