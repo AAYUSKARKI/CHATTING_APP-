1ece2ec975d88b9214562d75ff657a562504b328
AAYUS KARKI <alexkarki2060@gmail.com>
2024-05-11 16:11:40 +0545

    url

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,10 +1,13 @@
 import express, { urlencoded } from "express";
 import cors from 'cors';
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 const app = express();
+dotenv.config({path : './.env'})
+console.log(process.env.CORS_ORIGIN);
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
