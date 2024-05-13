cfc25a0650972753b83673eff643be9f0a76fa25
AAYUS KARKI <alexkarki2060@gmail.com>
2024-05-13 17:16:26 +0545

    testing...

--- FRONTEND/src/Components/Home.tsx
+++ FRONTEND/src/Components/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
     const {user} = useSelector((state:any)=>state.user)
   return (
     <>
-<div className='bg-slate-700'>
+<div className='bg-slate-700 overflow-auto'>
     <div className='flex items-center justify-center'>
         <h1 className='text-3xl text-white'>WELCOME {user.user.username}</h1>
     </div>
