4ea5df53f22747954b318d1934b65770704a653c
Sandesh laksamba <sandeshlaksamba123@gmail.com>
2024-05-14 16:40:39 +0545

    login ui change

--- FRONTEND/src/Components/Login.tsx
+++ FRONTEND/src/Components/Login.tsx
@@ -47,27 +47,27 @@ const Login = () => {
 
             <div className="h-screen flex flex-col items-center justify-center bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-100">
 
-                <h1 className='text-3xl text-blue-500 py-2 px-4'>Welcome To Chat App</h1>
+                <h1 className='text-3xl text-blue-600 py-2 px-4 font-serif font-bold'>Welcome To Chat App</h1>
 
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4 bg-black">
 
                     <label htmlFor="name">UserName</label>
                     <input
-                        className="rounded-md p-1 "
+                        className="rounded-md p-1 bg-gray-400 "
                         type="text" name="username"
                         value={user.username}
                         onChange={handleChange}
                         placeholder="Enter your username" />
 
                     <label htmlFor="email">Email</label>
-                    <input className="rounded-md p-1 "
+                    <input className="rounded-md p-1 bg-gray-400"
                         type="email" name="email"
                         value={user.email}
                         onChange={handleChange}
                         placeholder="Enter your email" />
 
                     <label htmlFor="password">Password</label>
-                    <input className="rounded-md p-1 "
+                    <input className="rounded-md p-1 bg-gray-400"
                         type='password'
                         name="password"
                         value={user.password}
@@ -75,8 +75,9 @@ const Login = () => {
                         placeholder="Enter your password" />
 
 
-                    <button disabled={loading} type="submit">Login</button>
-                    <p>Don`t have an account? <Link to="/">Register</Link></p>
+                    <button className='bg-blue-300  hover:bg-blue-500 rounded-md p-3 ' 
+                    disabled={loading} type="submit">Login</button>
+                    <p>Don`t have an account? <Link className='text-blue-500' to="/">Register</Link></p>
                 </form>
             </div>
 
